b28d377d34c8d72692722dd8b7e5bbd0ce8fb316
Devin Akman <akman2@illinois.edu>
2014-10-14 07:56:38 +0000

    initial commit

---
+++ launch.rb
@@ -0,0 +1,54 @@
+#!/usr/bin/env ruby
+
+require 'socket'
+require 'require_all'
+
+require_all 'plugins'
+
+bot = Cinch::Bot.new do
+    configure do |c|
+        c.nick = 'Luggy'
+        c.realname = 'LUG Bot'
+        c.user = 'luggy'
+        c.server = 'irc.freenode.net'
+        c.messages_per_second = 1
+        c.channels = %w[##uiuclug ##opennsm]
+        c.plugins.plugins = [RSSPlugin]
+    end
+end
+
+bot.loggers.clear
+
+bthread = Thread.new { bot.start }
+
+Signal.trap('INT') { exit }
+
+at_exit do
+    bot.quit
+    bthread.kill
+    File.delete('/tmp/luggy.socket')
+end
+
+UNIXServer.open('/tmp/luggy.socket') do |srv|
+    conn = srv.accept
+
+    loop do
+        cmd = conn.gets
+        exit if cmd.nil?
+
+        case cmd.chomp
+        when 'quit'
+            return
+        when 'restart'
+            bot.quit
+            bthread.kill
+            bthread = Thread.new { bot.start }
+        when /^nick (?<nick>.+)$/
+            bot.nick = $~[:nick]
+        when /^join (?<chan>.+)$/
+            bot.join($~[:chan])
+        when /^part (?<chan>.+)$/
+            bot.part($~[:chan])
+        end
+    end
+end
---
+++ plugins/rss.rb
@@ -0,0 +1,26 @@
+require 'cinch'
+require 'rss'
+
+class RSSPlugin
+    include Cinch::Plugin
+
+    match /([a-z-]+\s+\d+)$/
+
+    def execute(m, args)
+        args = args.split(/\s+/)
+        feed = args[0]
+        num = args[1].to_i
+        if num.zero?
+            m.reply(Format(:red, 'You must load at least one article.'))
+            return
+        end
+
+        begin
+            rss = RSS::Parser.parse("http://seclists.org/rss/#{feed}.rss", true)
+            fail if rss.nil?
+            rss.items.to_a[0..num.pred].each { |item| m.reply("#{item.title.gsub(/\s+/, ' ')} (#{item.link})") }
+        rescue
+            m.reply(Format(:red, "Could not load list #{feed}."))
+        end
+    end
+end
