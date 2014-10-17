e289198059d3b50825b3f95350fe89560b8090c9
Kyle Nusbaum <KyleJNusbaum@gmail.com>
2014-10-16 19:11:26 -0500

    Added factoid plugin

---
+++ plugins/factoid.rb
@@ -0,0 +1,73 @@
+require 'cinch'
+require 'sqlite3'
+
+$messages = Hash.new
+$factdb = SQLite3::Database.new "lugbot.db"
+
+begin
+    $factdb.execute <<-SQL
+        CREATE TABLE factoids (
+            name TEXT PRIMARY KEY,
+            factoid TEXT
+        );
+    SQL
+rescue
+end
+
+class FactAdd
+    include Cinch::Plugin
+    
+    match /factadd\s+([a-zA-Z0-9]+)\s+(.+)$/
+    
+    def execute(m, factname, factoid)
+        m.reply("Adding: #{factname}: #{factoid}")
+        begin
+            $factdb.execute("INSERT INTO factoids(name, factoid) VALUES (?, ?)", 
+                             [factname, factoid])
+            m.reply("Added factoid #{factname}", prefix=true)
+        rescue
+            m.reply("Couldn't add factoid #{factname}. Make sure a fact with that name doesn't already exist.", prefix=true)
+        end
+    end
+end
+
+class Factoid
+    include Cinch::Plugin
+
+    match /,([a-zA-Z0-9]+)\s*(.*)?$/, :use_prefix => false
+
+    def execute(m, factname, nick)
+        fact = $factdb.execute("select factoid from factoids 
+                              where name = ?", factname)
+        if fact.count == 0
+            m.reply("Factoid #{factname} not found.")
+        else
+            fact.each do |row|
+                if nick != ""
+                    m.reply("#{nick}: #{row[0]}")
+                else
+                    m.reply("#{row[0]}")
+                end
+            end
+        end
+    end
+end
+
+class FactDel
+    include Cinch::Plugin
+
+    match /factdel\s+([a-zA-Z0-9]+)/
+
+    def execute(m, factname)
+        fact = $factdb.execute("select factoid from factoids 
+                              where name = ?", factname)
+        if fact.count == 0
+            m.reply("Factoid #{factname} not found.", prefix=true)
+        else
+            m.reply("Deleting factoid #{factname}")
+            fact = $factdb.execute("delete from factoids
+                                where name = ?", factname)
+            m.reply("Deleted factoid #{factname}", prefix=true)
+        end
+    end
+end
