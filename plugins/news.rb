c186c985519f77395567b51e7ee2c2c63bba1c62
Devin Akman <akman2@illinois.edu>
2014-10-17 02:00:46 +0000

    added some more sources to news and put in optional os argument (linux, bsd, osx)

--- plugins/news.rb
+++ plugins/news.rb
@@ -5,11 +5,31 @@ require_relative '../lib/rssreader'
 class NewsPlugin
     include Cinch::Plugin
 
-    match 'news'
+    LINUX_NEWS = ['http://feeds.feedburner.com/linuxjournalcom?format=xml']
+    BSD_NEWS = ['http://www.bsdnewsletter.com/newsfeeds/bsdnewsletter.rdf']
+    OSX_NEWS = [
+        'http://feeds.feedburner.com/osxdaily?format=xml',
+        'http://www.macworld.com/index.rss'
+    ]
 
-    def execute(m)
+    match /news(?:\s+(.+))?$/
+
+    def execute(m, arg)
         begin
-            RSSReader.summary('http://feeds.feedburner.com/linuxjournalcom?format=xml')[0..9].each { |item| m.reply(item) }
+            case arg
+            when 'linux'
+                sources = LINUX_NEWS
+            when 'bsd'
+                sources = BSD_NEWS
+            when 'osx'
+                sources = OSX_NEWS
+            else
+                sources = LINUX_NEWS + BSD_NEWS + OSX_NEWS
+            end
+            news = []
+            sources.each { |source| news += RSSReader.getitems(source) }
+            news.sort! { |a, b| b.date.to_i <=> a.date.to_i }
+            RSSReader.format(news[0..9]).each { |n| m.reply(n) }
         rescue
             m.reply(Format(:red, 'Could not load news.'))
         end
