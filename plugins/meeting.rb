45e01507cb668928419752210dcebe083dc47605
Devin Akman <akman2@illinois.edu>
2014-10-20 20:43:03 +0000

    separated meeting info for two different channels

--- plugins/meeting.rb
+++ plugins/meeting.rb
@@ -8,7 +8,8 @@ class MeetingPlugin
 
     def execute(m)
         begin
-            info = YAML.load(File.read('notes.yml')) # notes.yml is updated frequently by a cron job (the GitHub URL is added to the local copy only)
+            info = YAML.load(File.read("#{m.channel == '##opennsm' ? 'nsm' : 'lug'}notes.yml"))
+            # both YAML files are updated frequently by a cron job (the GitHub URL is added to the local copy only)
             m.reply("#{m.user.nick}: Here is some info about our next meeting:")
             m.reply("It will be at #{info['Meeting Info']}.")
             m.reply("This week's big talk is about #{info['Talks']['Big Talks'].keys.first}.")
