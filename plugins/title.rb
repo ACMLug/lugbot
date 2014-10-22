2c68c387f21bdd55dfb83d4da3ecb5c5f98b67ad
Devin Akman <akman2@illinois.edu>
2014-10-22 03:28:57 +0000

    added format stripping to url match

--- plugins/title.rb
+++ plugins/title.rb
@@ -6,7 +6,7 @@ class TitlePlugin
     #** Automatically fetches the titles of URLs posted in chat **#
     include Cinch::Plugin
 
-    match /(#{URI.regexp(%w[http https])})/, use_prefix: false
+    match /(#{URI.regexp(%w[http https])})/, use_prefix: false, strip_colors: true
 
     def execute(m, uri)
         m.reply(Nokogiri::HTML(URI(uri).read).search('title').first.content) rescue nil
