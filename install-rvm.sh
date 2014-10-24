43c2ac1c97a5d030df749540d55a108550cf8cc4
Devin Akman <awesome9dude@gmail.com>
2014-10-24 04:13:50 -0400

    fixed Vagrant provisioning script

--- install-rvm.sh
+++ install-rvm.sh
@@ -1,6 +1,16 @@
- #!/usr/bin/env bash
+#!/usr/bin/env bash
+
+apt-get -y install build-essential ruby-dev fortune
 
 curl -sSL https://get.rvm.io | bash -s $1
 
-cd /vagrant
-bundle install
+/usr/local/rvm/bin/rvm install ruby
+
+gem install bundler
+
+usermod -a -G rvm vagrant
+
+su - vagrant -c 'echo "source /etc/profile.d/rvm.sh" >> /home/vagrant/.bashrc'
+su - vagrant -c 'source /home/vagrant/.bashrc'
+su - vagrant -c 'rvm use --default ruby'
+su - vagrant -c 'cd /vagrant; bundle install'
